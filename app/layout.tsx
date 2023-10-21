e9534c15d03447cbd0ac1c3a59f9b902b0b1e3da
ChaPerx64 <chayan.par@gmail.com>
2023-10-21 12:34:29 +0200

    Mobile version implemented

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   const [currSlide, setSlide] = useState(0);
   const slideref = useRef(null);
   const [displayedCard, setDisplayedCard] = useState(currSlide);
-  const changingclass = "translate-x-3/4";
+  const changingclass = "translate-x-full";
   switchelementanimation({
     ref: slideref,
     setState: setDisplayedCard,
@@ -32,38 +32,16 @@ export default function RootLayout({
   });
   return (
     <html lang="en" className={htmlClassName}>
-      <body className="h-auto md:h-screen overflow-hidden">
+      <body className="h-auto md:h-screen overflow-x-hidden md:overflow-y-hidden">
         <Providers>
           <MyNavbar isBordered height="6vh" data={data} setslide={setSlide} />
           <main className={"static h-auto md:h-[93vh] w-full"}>
             <div
               className={
-                "h-1/2 w-full md:h-full md:w-1/2 flex items-center justify-center"
+                "md:absolute mt-5 bottom-5 left-0 \
+                z-50 w-full flex items-center justify-center"
               }
             >
-              <BgSlider
-                images={data.sections.map(({ img }) => img)}
-                currSlide={currSlide}
-              />
-            </div>
-            <div
-              className={`absolute top-auto bottom-0 left-0 h-[93vh] w-full \
-                flex flex-row items-center justify-center \
-                transition-transform ${changingclass}`}
-              ref={slideref}
-            >
-              <div className={"flex-basis-5/6 w-5/6"}>
-                <CardsGenerator
-                  className={"w-1/2 ml-auto"}
-                  sections={data.sections.map(({ header, text }) => ({
-                    header,
-                    text,
-                  }))}
-                  state={[displayedCard, setSlide]}
-                />
-              </div>
-            </div>
-            <div className="absolute bottom-5 left-0 w-full flex items-center justify-center">
               <Pagination
                 showShadow
                 total={data.sections.length}
@@ -72,6 +50,34 @@ export default function RootLayout({
                   setSlide(n - 1);
                 }}
               />
+            </div>
+            <div
+              className={
+                "w-full md:h-full md:w-1/2 flex z-0 items-center justify-center"
+              }
+            >
+              <BgSlider
+                images={data.sections.map(({ img }) => img)}
+                currSlide={currSlide}
+              />
+            </div>
+            <div
+              className={`md:absolute md:bottom-0 md:left-0 my-7 md:my-auto \
+                h-auto md:h-[93vh] w-full \
+                flex flex-row items-center justify-center \
+                transition-transform ${changingclass}`}
+              ref={slideref}
+            >
+              <div className={"flex-basis-5/6 w-5/6"}>
+                <CardsGenerator
+                  className={"w-full md:w-1/2 ml-auto"}
+                  sections={data.sections.map(({ header, text }) => ({
+                    header,
+                    text,
+                  }))}
+                  state={[displayedCard, setSlide]}
+                />
+              </div>
             </div>
           </main>
         </Providers>
